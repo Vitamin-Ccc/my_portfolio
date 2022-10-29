57bf61f49f649f3d0820ea8c348539bb53eb6d9d
Sky Lin <linsky519@gmail.com>
2022-10-29 10:56:13 -0600

    index.css file completed for now. Still picking colors

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+import Resume from "./Resume";
 
 const Header = () => {
   return (
-    <div>
-      <h5>Hey! Welcome to</h5>
-      <h1>Sky Lin</h1>
-      <h5>'s Portfolio</h5>
-    </div>
-  )
-}
+    <header>
+      <div>
+        <h5>Hey! Welcome to</h5>
+        <h1>Sky Lin</h1>
+        <h5>'s Portfolio</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Resume />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/header/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className='resume'>
+      <a href="" download>Check Out My Resume</a>
+      <a href="">Let's Chat</a>
+    </div>
+  )
+}
+
+export default Resume
