ccd342d4a59816026caa17879cf141e8d9405989
Sky Lin <linsky519@gmail.com>
2022-10-31 20:53:31 -0600

    contact section is done, need to work on resize.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import "./contact.css"
+import React, { useRef } from "react";
+import "./contact.css";
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ruij00h",
+        "template_5cld0ao",
+        form.current,
+        "EE_RT39LxYD82YUMS"
+      )
+      e.target.reset()
+  };
   return (
     <section id="contact">
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
